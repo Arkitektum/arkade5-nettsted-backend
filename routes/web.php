15b38fd34b62fcf3a6ead868dff1669a7ba2d11b
Jørgen Tellnes <jorgen@arkitektum.no>
2025-08-06 16:53:26 +0200

    Integrate frontend with backend

--- routes/web.php
+++ routes/web.php
@@ -32,106 +32,113 @@ use Illuminate\Support\Facades\Storage;
 |
 */
 
+Route::domain('backend.' . env('APP_URL'))->group(function () {
+    
+    Route::get('/', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    Route::middleware('auth')->prefix('statistikk')->name('statistics.')->group(function () {
+
+        Route::get('/', function () {
+            return view('statistics.index',
+                ['links' => [
+                    'arkadenedlastinger' => route('statistics.downloads'),
+                    'arkadenedlastere' => route('statistics.downloaders'),
+                    'arkadenedlaster-organisasjoner' => route('statistics.organizations'),
+                    'arkadeutgivelser' => route('statistics.releases'),
+                    //'self' => route('statistics.index'),
+                ]]);
+        })->name('index');
+
+        Route::get('arkade-nedlastinger', function (Request $request) {
+            $downloads = ($releaseId = $request->input('utgivelse'))
+                ? ArkadeDownload::orderByDesc('downloaded_at')->whereArkadeReleaseId($releaseId)->paginate()
+                : ArkadeDownload::orderByDesc('downloaded_at')->paginate();
+            return view('statistics.arkade-downloads.index', [
+                'downloads' => new ArkadeDownloadCollection($downloads),
+                'totalCount' => ArkadeDownload::count()
+            ]);
+        })->name('downloads');
+
+        Route::get('arkade-nedlastinger/{download}', function (ArkadeDownload $download) {
+            return new ArkadeDownloadResource(ArkadeDownload::find($download->id));
+        })->name('download');
+
+        Route::get('arkade-utgivelser', function () {
+            return view('statistics.arkade-releases.index', [
+                'releases' => new ArkadeReleaseCollection(ArkadeRelease::withoutGlobalScope('public')->orderByDesc('released_at')->paginate()),
+                'totalCount' => ArkadeRelease::withoutGlobalScope('public')->count()
+            ]);
+        })->name('releases');
+
+        Route::get('arkade-utgivelser/{release}', function (ArkadeRelease $release) {
+            return new ArkadeReleaseResource(ArkadeRelease::find($release->id)); // TODO: Disable global scope 'public'
+        })->name('release');
+
+        Route::get('arkade-nedlastere', function () {
+            return view('statistics.arkade-downloaders.index', [
+                'downloaders' => new ArkadeDownloaderCollection(ArkadeDownloader::orderByDesc('created_at')->paginate()),
+                'totalCount' => ArkadeDownloader::count()
+            ]);
+        })->name('downloaders');
+
+        Route::get('arkade-nedlastere/{downloader}', function (ArkadeDownloader $downloader) {
+            return new DownloaderResource(ArkadeDownloader::find($downloader->id));
+        })->name('downloader');
+
+        Route::get('organisasjoner', function () {
+            return view('statistics.arkade-organizations.index', [
+                'organizations' => new OrganizationCollection(Organization::orderByDesc('created_at')->paginate()),
+                'totalCount' => Organization::count()
+            ]);
+        })->name('organizations');
+
+        Route::get('organisasjoner/{organization}', function (Organization $organization) {
+            return new OrganizationResource(Organization::find($organization->id));
+        })->name('organization');
+    });
+
+    Route::middleware('auth')->prefix('builds')->name('builds.')->group(function () {
+
+        Route::get('/', function () {
+            return view('builds.index', ['buildTypes' => array_map('basename', Storage::directories('builds'))]);
+        })->name('index');
+
+        Route::get('/{buildType}', function ($buildType) {
+            return view('builds.build-list', [
+                'buildType' => $buildType,
+                'builds' => Storage::files('builds/' . $buildType)
+            ]);
+        })->name('buildList');
+
+        Route::get('/{buildType}/{build}', function ($buildType, $build) {
+            return Storage::download('builds/' . $buildType . '/' . $build);
+        })->name('buildDownload');
+    });
+
+    Route::middleware('auth')->get('news-receivers', function () {
+
+        $newsReceiverEmails = ArkadeDownloader::whereWantsNews(true)->pluck('email');
+        $latestArkadeVersionNumber = ArkadeRelease::orderBy('released_at', 'desc')->get()
+            ->unique('version_number')->pluck('version_number')->first();
+
+        return view('news-receivers.index', [
+            'newsReceiverEmails' => $newsReceiverEmails->toArray(),
+            'numberOfNewsReceivers' => $newsReceiverEmails->count(),
+            'latestArkadeVersionNumber' => $latestArkadeVersionNumber
+        ]);
+    })->name('newsReceivers');
+
+    require __DIR__.'/auth.php';
+});
+
 Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+   return response()->file('frontend/index.html');
 });
-
-Route::middleware('auth')->prefix('statistikk')->name('statistics.')->group(function () {
-
-    Route::get('/', function () {
-        return view('statistics.index',
-            ['links' => [
-                'arkadenedlastinger' => route('statistics.downloads'),
-                'arkadenedlastere' => route('statistics.downloaders'),
-                'arkadenedlaster-organisasjoner' => route('statistics.organizations'),
-                'arkadeutgivelser' => route('statistics.releases'),
-                //'self' => route('statistics.index'),
-            ]]);
-    })->name('index');
-
-    Route::get('arkade-nedlastinger', function (Request $request) {
-        $downloads = ($releaseId = $request->input('utgivelse'))
-            ? ArkadeDownload::orderByDesc('downloaded_at')->whereArkadeReleaseId($releaseId)->paginate()
-            : ArkadeDownload::orderByDesc('downloaded_at')->paginate();
-        return view('statistics.arkade-downloads.index', [
-            'downloads' => new ArkadeDownloadCollection($downloads),
-            'totalCount' => ArkadeDownload::count()
-        ]);
-    })->name('downloads');
-
-    Route::get('arkade-nedlastinger/{download}', function (ArkadeDownload $download) {
-        return new ArkadeDownloadResource(ArkadeDownload::find($download->id));
-    })->name('download');
-
-    Route::get('arkade-utgivelser', function () {
-        return view('statistics.arkade-releases.index', [
-            'releases' => new ArkadeReleaseCollection(ArkadeRelease::withoutGlobalScope('public')->orderByDesc('released_at')->paginate()),
-            'totalCount' => ArkadeRelease::withoutGlobalScope('public')->count()
-        ]);
-    })->name('releases');
-
-    Route::get('arkade-utgivelser/{release}', function (ArkadeRelease $release) {
-        return new ArkadeReleaseResource(ArkadeRelease::find($release->id)); // TODO: Disable global scope 'public'
-    })->name('release');
-
-    Route::get('arkade-nedlastere', function () {
-        return view('statistics.arkade-downloaders.index', [
-            'downloaders' => new ArkadeDownloaderCollection(ArkadeDownloader::orderByDesc('created_at')->paginate()),
-            'totalCount' => ArkadeDownloader::count()
-        ]);
-    })->name('downloaders');
-
-    Route::get('arkade-nedlastere/{downloader}', function (ArkadeDownloader $downloader) {
-        return new DownloaderResource(ArkadeDownloader::find($downloader->id));
-    })->name('downloader');
-
-    Route::get('organisasjoner', function () {
-        return view('statistics.arkade-organizations.index', [
-            'organizations' => new OrganizationCollection(Organization::orderByDesc('created_at')->paginate()),
-            'totalCount' => Organization::count()
-        ]);
-    })->name('organizations');
-
-    Route::get('organisasjoner/{organization}', function (Organization $organization) {
-        return new OrganizationResource(Organization::find($organization->id));
-    })->name('organization');
-});
-
-Route::middleware('auth')->prefix('builds')->name('builds.')->group(function () {
-
-    Route::get('/', function () {
-        return view('builds.index', ['buildTypes' => array_map('basename', Storage::directories('builds'))]);
-    })->name('index');
-
-    Route::get('/{buildType}', function ($buildType) {
-        return view('builds.build-list', [
-            'buildType' => $buildType,
-            'builds' => Storage::files('builds/' . $buildType)
-        ]);
-    })->name('buildList');
-
-    Route::get('/{buildType}/{build}', function ($buildType, $build) {
-        return Storage::download('builds/' . $buildType . '/' . $build);
-    })->name('buildDownload');
-});
-
-Route::middleware('auth')->get('news-receivers', function () {
-
-    $newsReceiverEmails = ArkadeDownloader::whereWantsNews(true)->pluck('email');
-    $latestArkadeVersionNumber = ArkadeRelease::orderBy('released_at', 'desc')->get()
-        ->unique('version_number')->pluck('version_number')->first();
-
-    return view('news-receivers.index', [
-        'newsReceiverEmails' => $newsReceiverEmails->toArray(),
-        'numberOfNewsReceivers' => $newsReceiverEmails->count(),
-        'latestArkadeVersionNumber' => $latestArkadeVersionNumber
-    ]);
-})->name('newsReceivers');
-
-require __DIR__.'/auth.php';
